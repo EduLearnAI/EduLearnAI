89ed93d5e896346fbed1d2c0724c9c89d96207ba
EduLearnAI <edulearnai@gmail.com>
2026-01-18 00:25:53 +0500

    Update unibot.jsx

--- src/pages/unibot.jsx
+++ src/pages/unibot.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import { FiSend, FiPaperclip } from 'react-icons/fi'
 
-const backendBaseUrl = 'https://mominah-edulearnai.hf.space'
+const backendBaseUrl = 'https://edulearnai-edulearnai.hf.space'
 
 export default function UniBot() {
   const [messages, setMessages] = useState([])
